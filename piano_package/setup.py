fe7f4c302542c8e686fc243c7efdc1266d52048d
Fridtjof Petersen <65158001+petersen-f@users.noreply.github.com>
2021-05-05 23:54:20 +0200

    update Readme

---
+++ piano_package/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name= "piano_package",
+    version= "0.0.1",
+    author= "Fridtjof",
+    author_email= "fridtjof.petersen@student.uva.nl",
+    python_requires = ">=3.6",
+    packages = setuptools.find_packages()
+)
