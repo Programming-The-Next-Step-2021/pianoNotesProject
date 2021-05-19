c7dd4a659fff2d0b1bd4b3e1a0de24b02d6cf407
Fridtjof Petersen <65158001+petersen-f@users.noreply.github.com>
2021-05-19 12:13:24 +0200

    created tests

--- Test.py
+++ Test.py
@@ -1,14 +1,8 @@
 # This python file serves as a test for the package as it imports it and can be used to launch the progran
 
-
-import unittest
 import piano_package as pp
 
-unittest discover 
 # pp.test_run(device="piano1")
-
-device, config_file_df, path = pp.setup_midi(device="launchpad")
-
 
 
 # piano.test_run()
